75d43924940f3133151bc2ffa2e85f2c8be65e78
SHIBATA <ookinakurino3@gmail.com>
2022-08-25 15:50:15 +0900

    a

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace CS17
+{
+    class Customer
+    {
+       static void Main()
+        {
+
+        }
+        
+        public string FirstName;
+        public string LastName;
+      
+        ///
+        /// コンストラクタ（フルネーム指定）
+        /// 
+        ///名
+        ///姓
+        public Customer(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        ///
+        /// コンストラクタ（苗字指定）
+        /// 
+        ///姓
+        public Customer(string lastName) : this("--", lastName)
+        {
+            //コンストラクタイニシャライザによりここに重複コードを書く必要がなくなる
+            ////FirstName = "--";
+            ////LastName = lastName;
+        }
+
+        ///
+        /// フルネームの取得
+        /// 
+        /// 
+        public string GetFullName()
+        {
+            return LastName + FirstName;
+        }
+       
+    }
+}
--- ConsoleApp11/Program.cs
+++ ConsoleApp11/Program.cs
@@ -1,2 +1,92 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public class box
+{
+    static void Main()
+    {
+        float Ai;
+        while (true)
+        {
+            Ai = InputFloat($"底辺の長さと高さで計算するなら1　三辺の長さで計算するなら2");
+            if (Ai == 1)
+            {
+                Box box = new Box(InputFloat("底辺の長さを入力"), InputFloat("底面の高さを入力"), InputFloat("柱の高さを入力"));
+                Console.WriteLine("三角柱の表面積 = " + box.GetSurfaceC() + " 体積 = " + box.GetVolumeC());
+                break;
+            }
+            if(Ai == 2)
+            {
+                Box box = new X(InputFloat("底辺の長さを入力"), InputFloat("底面の高さを入力"), InputFloat("柱の高さを入力"));
+                Console.WriteLine("三角柱の表面積 = " + box.GetSurfaceC() + " 体積 = " + box.GetVolumeC());
+                break;
+            }
+        }
+        
+    }
+
+    static float InputFloat(string su)
+    {
+        float input;
+        while (true)
+        {
+            Console.WriteLine(su);
+            if (float.TryParse(Console.ReadLine(), out input))
+            {
+                return input;
+            }
+        }
+    }
+}
+class Box
+{
+    private float length;
+    private float height;
+    private float pillar;
+    public Box(float l, float h, float p)
+    {
+        this.length = l;
+        this.height = h;
+        this.pillar = p;
+
+    }
+    public Box(float r = 1)
+    {
+        this.length = r;
+        this.height = r;
+        this.pillar = r;
+    }
+    public float GetVolumeC()
+    {
+        return (length*height)/2*pillar;
+    }
+    public float GetSurfaceC()
+    {
+        return ((float)(length * 3.14 * height * 2));
+    }
+}
+class X
+{
+    private float length;
+    private float height;
+    private float pillar;
+    public X(float l, float h, float p)
+    {
+        this.length = l;
+        this.height = h;
+        this.pillar = p;
+
+    }
+    public X(float r = 1)
+    {
+        this.length = r;
+        this.height = r;
+        this.pillar = r;
+    }
+    public float GetVolumeC()
+    {
+        return (length * height) / 2 * pillar;
+    }
+    public float GetSurfaceC()
+    {
+        return ((float)(length * 3.14 * height * 2));
+    }
+}
